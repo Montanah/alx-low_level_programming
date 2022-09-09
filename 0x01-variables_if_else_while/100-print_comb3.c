2989ddbfc01f38786faedfcde35e4bff694169d0
Montanah <mititi.isaac@gmail.com>
2022-09-09 15:37:42 -0700

    100-Print

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 30; i <= 19; i++)
+	for (i = 10; i <= 19; i++)
 	{
 		for (j = 10; j <= 19; j++)
 		{
 			putchar((i % 10) + '0');
 			putchar((i % 10) + '0');
-			if (i != 19 || j !=19)
+			if (i != 19 || j != 19)
 			{
 				putchar(',');
 				putchar(' ');
