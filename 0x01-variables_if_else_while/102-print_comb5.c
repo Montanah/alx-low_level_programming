43a998dcf4591b0c141b7106de278ae1d3a3d58b
Montanah <mititi.isaac@gmail.com>
2022-09-10 05:14:05 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,14 +16,14 @@ int main(void)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
-			putchar('');
+			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 
 			if (!(i == 98 && j == 99))
 			{
 				putchar(',');
-				putchar('');
+				putchar(' ');
 			}
 		}
 	}
