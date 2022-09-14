132a69c2789e582f40bb8d8f97554f960d2ab82a
Montanah <mititi.isaac@gmail.com>
2022-09-14 16:20:37 -0700

    Last digit print commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "6-abs.c"
+
+/**
+ * prints_last_digit - main function
+ *
+ * @r: ineger to get last digit of number
+ *
+ * Return: last digit of r
+ */
+int print_last_digit(int r)
+{
+	_putchar('0' + _abs(r % 10));
+	return (_abs(r % 10));
+}
