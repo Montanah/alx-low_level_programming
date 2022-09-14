c8324f3b4c7c1a94a0418b807a3f6aab6799fc5d
Montanah <mititi.isaac@gmail.com>
2022-09-14 16:05:45 -0700

    Absolute interger commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ * @j: integer to make absolute
+ *
+ *Return: int or negative int
+ */
+
+int _abs(int j)
+{
+	if (j > 0)
+		return (j);
+	else 
+		return (j * -1);
+}
