5719d4f83dcb3afc7b9e8e2dd6c0dbae1ea89013
Montanah <mititi.isaac@gmail.com>
2022-09-15 14:07:07 -0700

    2-mul.c commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplier
+ *
+ * @a: int to mult
+ *
+ * @b: int to mult
+ *
+ * Return: result of mult  of the two
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
