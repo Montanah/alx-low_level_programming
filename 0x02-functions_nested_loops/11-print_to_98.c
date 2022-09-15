258e3f1fb6bf5db42076eb4dd89f35f4179f6b28
Montanah <mititi.isaac@gmail.com>
2022-09-15 06:01:20 -0700

    11-print_to_98.c commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include "6-abs.c"
+
+/**
+ * prints_natural_numbers - n to 98
+ *
+ * @n: starting number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf('\n');	
+}
