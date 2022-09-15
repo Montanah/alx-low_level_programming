5f7cc937261880cfcf2bf061d9d89f75e175a123
Montanah <mititi.isaac@gmail.com>
2022-09-15 05:40:31 -0700

    Addition commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adding two integers
+ *
+ * @i: first integer
+ *
+ * @j: second integer
+ *
+ * Return: sum of the two integers
+ */
+int add(int i, int j)
+{
+	return(i + j);
+}
