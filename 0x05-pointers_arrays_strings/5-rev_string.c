0e93d6a02df37534aa4c58e99266d7aec7c14e25
Montanah <mititi.isaac@gmail.com>
2022-09-19 14:17:37 -0700

    5-rev_string.c commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string - reverses a string value
+ *
+ * @s: string to print
+ */
+void rev_string(char *s)
+{
+	int j;
+	char u;
+
+	for (j = 0; j <= _strlen(s) / 2; j++)
+	{
+		u = s[j];
+		s[j] = s[_strlen(s) - j - 1];
+		s[_strlen(s) - i - 1] = t;
+	}
+}
