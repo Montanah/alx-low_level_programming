49663c42364b3a5219454564175481540ecfb338
Montanah <mititi.isaac@gmail.com>
2022-09-19 15:24:34 -0700

    9-strcpy.c commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - a function that copies the string pointed to
+ *
+ * @dest: destination to copy to
+ *
+ * @src: src
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
