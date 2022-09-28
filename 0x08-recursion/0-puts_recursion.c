1eab1cb73f34f72f01b2738cce1570b7492318a3
Montanah <mititi.isaac@gmail.com>
2022-09-28 13:46:26 -0700

    Recursion 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: string to recurse through
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_putchar(++s);
+	}
+	else 
+		_putchar('\n');
+}
