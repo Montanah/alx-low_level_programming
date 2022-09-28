e6ceda67ceccf8f52cf7fb2d4f6edc0d260163cf
Montanah <mititi.isaac@gmail.com>
2022-09-28 14:07:22 -0700

    Recursion 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ *
+ * @s: string to use
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s)
+	{
+		n = _strlen_recursion(s + 1);
+		return (n += 1);
+	}
+	retrun (0);
+}
