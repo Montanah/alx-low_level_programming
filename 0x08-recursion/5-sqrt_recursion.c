f28f6b6f907c1952fea16daadf8dc6baef5ee1fa
Montanah <mititi.isaac@gmail.com>
2022-09-29 04:48:15 -0700

    Recursion 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * helper - helps decide if right
+ *
+ * @i: integer to randomly put
+ *
+ * @n: integer to get root of
+ *
+ * Return: value of root
+ */
+int helper(int i, int n)
+{
+	int j;
+
+	if (i * i != n)
+	{
+		if (i > n)
+		{
+			return (-1);
+		}
+		j = helper(i + 1, n);
+		return (j + 1);
+	}
+	return (0);
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number
  *
  * @n: integer to get root of
