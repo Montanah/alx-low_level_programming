95e54a67c24d4fe8e8bc55a96a7d5fecf9080a01
Montanah <mititi.isaac@gmail.com>
2022-10-04 14:30:26 -0700

    Trailing whitespace removal

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -12,11 +12,9 @@ void free_grid(int **grid, int height)
 	int i;
 	
 	if (height <= 0)
-		return;
-	
+		return;	
 	if (grid == NULL)
 		return;
-	
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
