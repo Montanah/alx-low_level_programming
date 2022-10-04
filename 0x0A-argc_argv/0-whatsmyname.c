1b216f6fa16069ce583dbd02b1014527b0f6c8de
Montanah <mititi.isaac@gmail.com>
2022-10-03 23:51:55 -0700

    Task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - main function
+ *
+ * @argc - argument count
+ *
+ * @argv - argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
+}
