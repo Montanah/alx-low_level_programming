7b059972c9f87679fcd9c598cf9d85a9667e7f78
Montanah <mititi.isaac@gmail.com>
2022-10-05 08:44:40 -0700

    Comments edit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,8 +2,8 @@
 
 /**
  * main - main function
- * @argc - argument count
- * @argv - argument vector
+ * @argc: argument count
+ * @argv: argument vector
  * Return: always 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
