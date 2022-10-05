6a645d6bdb6d743d0560a5f21bd95ae910bf263b
Montanah <mititi.isaac@gmail.com>
2022-10-05 01:39:24 -0700

    Unused variable edit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int __attribute__((unused)) argc, char *argv[]);
-int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
