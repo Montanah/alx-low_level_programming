99de88941e28e01dd33c9c58aa987113ed4e9d03
Montanah <mititi.isaac@gmail.com>
2022-10-05 01:56:36 -0700

    Task 2 commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Reeturn: always 0
+ */
+int main(int agrc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
