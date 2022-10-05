237dd963e26ada57ad51b373c5074534a57bb82d
Montanah <mititi.isaac@gmail.com>
2022-10-05 01:51:01 -0700

    edit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#define UNUSED(x) (void) (x)
 
 /**
  * main - main function
- *
  * @argc - argument counter
- *
  * @argv - argument vector
- *
  * Return: always 0
  */
-int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[] __attribute__((__unused__)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
