fbb1322c36f57d88acbcfe78b8812f9ae0a80a20
Montanah <mititi.isaac@gmail.com>
2022-10-10 09:04:39 -0700

    Task 7 commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * leet - encodes string
+ * @s: string
+ * Return: char value
+ */
+char *leet(char *s)
+{
+	char alphaArr[] = "a4AA4e3E3o0O0t7T7l1L1";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alphaArr[j] != '\0'; j++)
+		{
+			if (s[i] == alphaArr[j])
+			{
+				s[i] = alphaArr[j + 1];
+				break;
+			}
+		}
+	}
+	return (s);
+}
