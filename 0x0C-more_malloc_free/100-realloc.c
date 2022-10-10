e1ccce5d9952cb2e6f43a9a90a0b81baead04df2
Montanah <mititi.isaac@gmail.com>
2022-10-10 05:58:12 -0700

    Task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+* _realloc - reallocates memory
+* @ptr: points to memory previously allocated
+* old_size: old size allocated
+* ne_size: new size allocated
+* Return: integer value
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *nptr;
+	unsigned int i;
+
+	if (new_zie == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		if (nptr == NULL)
+			return (NULL);
+		return (nptr);
+	}
+	else 
+	{
+		if (new_size == 0)
+		{
+			free (ptr);
+			retrun (NULL);
+		}
+	}
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && new_size; i++)
+	{
+		nptr[i] = ((char) * ptr)[i];
+	}
+	free (ptr);
+	return (ptr);
+}
