4e1dce38d12208d3cd126ac8b022e9a729831892
Montanah <mititi.isaac@gmail.com>
2022-10-10 09:24:37 -0700

    Task 8 commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - ecnodes a string using rot13
+ * @s: string to use
+ * Return: char value
+ */
+char *rot13(char *)
+{
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+
+	int i, j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; part1[j] != '\0'; j++)
+		{
+			if (s[i] == part1[j])
+			{
+				s[i] = part2[j];
+				break;
+			}
+		}
+	}
+}
