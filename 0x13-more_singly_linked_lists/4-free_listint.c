f1824ccb06fd5384af7ee52668f504fd9392f548
Montanah <mititi.isaac@gmail.com>
2022-10-27 15:00:16 -0700

    4-free_listint.c edit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,10 +7,14 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (head != NULL)
 	{
 		tmp = head;
 		free(head);
 		head = tmp->next;
 	}
+	free(head);
 }
