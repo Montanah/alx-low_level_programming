6b328f2c05379c12c4697e459e6c17e91c2a1cd5
Montanah <mititi.isaac@gmail.com>
2022-12-09 05:54:22 -0800

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(dlistint_t));
-	if (head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
