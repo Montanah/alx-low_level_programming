df5c41d85e9a57b53187bc6c0f9f68258eaf2e33
Montanah <mititi.isaac@gmail.com>
2023-01-14 09:27:32 -0800

    Task 9 edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,13 +8,13 @@ void times_table(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			int prod = j * i;
-			
+
 			if (j == 0)
 			{
 				_putchar('0');
