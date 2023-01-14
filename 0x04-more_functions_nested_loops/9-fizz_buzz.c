34224f566722247540af505bfbe0582b8baef48b
Montanah <mititi.isaac@gmail.com>
2023-01-14 12:17:38 -0800

    Betty edit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,14 +16,17 @@ int main(void)
 			if (i % 3 == 0)
 			{
 				printf("Fizz");
-			} if (i % 5 == 0)
+			}
+			if (i % 5 == 0)
 			{
 				printf("Buzz");
 			}
-		} else
+		}
+		else
 		{
 			printf("%d", i);
-		} if (i != 100)
+		}
+		if (i != 100)
 			printf(" ");
 	}
 	printf("\n");
