6767c63d451d1e2458198ef2bed5d3922e134165
Montanah <mititi.isaac@gmail.com>
2023-01-14 12:22:22 -0800

    Betty edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,7 +14,8 @@ void print_number(int n)
 	if (n == 0)
 	{
 		_putchar('0');
-	} if (n < 0)
+	}
+	if (n < 0)
 	{
 		_putchar('-');
 		n = n + 1;
@@ -23,14 +24,17 @@ void print_number(int n)
 		x = n;
 		x += 1;
 		y += 1;
-	} while (x != 0)
+	}
+	while (x != 0)
 	{
 		x = x / 10;
 		c++;
-	} for (i = 1; i < c; i++)
+	}
+	for (i = 1; i < c; i++)
 	{
 		d *= 10;
-	} for (i = 0; i < c; i++)
+	}
+	for (i = 0; i < c; i++)
 	{
 		_putchar(y / d + '0');
 		y = y % d;
