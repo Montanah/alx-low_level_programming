e2e636e2849fc86eaaea76660c75a82390e2b476
Montanah <mititi.isaac@gmail.com>
2023-02-24 07:01:23 -0800

    pycodestyle edit and other previous files commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,19 +9,19 @@ no diagonals
 
 
 def island_perimeter(grid):
-        ''' computes perimeter of island '''
+    ''' computes perimeter of island '''
 
-        perimeter = 0
+    perimeter = 0
 
-        for i in range(len(grid)):
-                for j in range(len(grid[0])):
-                        if grid[i][j] == 1:
-                                if j - 1 < 0 or grid[i][j-1] == 0:
-                                        perimeter += 1
-                                if j + 1 >= len(grid[0]) or grid[i][j+1] == 0:
-                                        perimeter += 1
-                                if i - 1 < 0 or grid[i-1][j] == 0:
-                                        perimeter += 1
-                                if i + 1 >= len(grid) or grid[i+1][j] == 0:
-                                        perimeter += 1
-        return perimeter
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if j - 1 < 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j + 1 >= len(grid[0]) or grid[i][j+1] == 0:
+                    perimeter += 1
+                if i - 1 < 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i + 1 >= len(grid) or grid[i+1][j] == 0:
+                    perimeter += 1
+    return perimeter
