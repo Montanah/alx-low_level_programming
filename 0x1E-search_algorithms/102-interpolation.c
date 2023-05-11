226033699a772e61bb6f44557bac5ad234e5472b
Montanah <mititi.isaac.com>
2023-05-11 13:03:25 -0700

    Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * Interpolation_search - finds a value by estimating where the value should be
+ * interpolation_search - finds a value by estimating where the value should be
  * @array: a pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
@@ -10,52 +10,42 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t left = 0;
-	size_t right  = size - 1;
-	size_t pos;
+	size_t left = 0, right  = size - 1, pos;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
-
 	pos = left + (((double)(right - left) / (array[right] - array[left]))
 		     * (value - array[left]));
 
 	while (1)
 	{
-
 		if (pos >= size)
 		{
 			printf("Value checked array[%u] is out of range\n", (unsigned int)pos);
 			return (-1);
 		}
-
 		printf("Value checked array[%u] = [%i]\n", (unsigned int)pos, array[pos]);
 
 		if (array[pos] == value)
 		{
 			return (pos);
 		}
-
 		if (array[pos] > value)
 		{
 			right = pos - 1;
 		}
-
 		else
 		{
 			left = pos + 1;
 		}
-
 		if (right <= left)
 		{
 			return (-1);
 		}
-
 		pos = left + (((double)(right - left) / (array[right] - array[left]))
 			* (value - array[left]));
 	}
-
 	return (-1);
 }
